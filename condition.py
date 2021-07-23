b27f0e088ec69acdac45cebc3ae791b71fd1a3f2
Tanapat <37849prc@gmail.com>
2021-07-23 09:07:12 +0700

    Create condition.py

---
+++ condition.py
@@ -0,0 +1,7 @@
+num1 = int(input("Enter you first num"))
+num2 = int(input("Enter your second num"))
+num3 = int(input("Enter your third num"))
+if(num1 > num2 and num1 < num3):
+    print("condition is True")
+else:
+    print("condition is not")
