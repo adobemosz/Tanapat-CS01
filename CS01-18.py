da266f279af56fa45c461b1b71c9c658529d545e
Tanapat <37849prc@gmail.com>
2021-09-17 10:00:01 +0700

    Create CS01-18.py

---
+++ CS01-18.py
@@ -0,0 +1,7 @@
+from tkinter import *
+root = Tk()
+root.title("First GUI")
+myText = Label(text="My name is ",fg="blue",font=20).grid(row=0,column=0)
+myText = Label(text="Tanapat ",fg="green",font=20).grid(row=1,column=1)
+myText = Label(text="Charunworaphan ",fg="red",font=20).grid(row=2,column=2)
+root.mainloop()
